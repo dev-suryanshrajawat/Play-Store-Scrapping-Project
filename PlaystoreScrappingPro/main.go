72961bfaf07c054f55050ee799cdf283d5d8ca00
dev-suryanshrajawat <suryanshrajawat.pro@gmail.com>
2025-11-17 16:16:08 +0530

    Update main.go

--- PlaystoreScrappingPro/main.go
+++ PlaystoreScrappingPro/main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"fmt"
 	"strings"
@@ -14,9 +15,8 @@ import (
 	"github.com/gin-gonic/gin"
 
 	tollbooth "github.com/didip/tollbooth/v7"
-	tollbooth_gin "github.com/didip/tollbooth/v7/gin"
+	tollbooth_gin "github.com/didip/tollbooth_gin"
 )
-
 
 ///////////////////////////////////////////////////////////////////////////////
 // SECURITY — sanitize and validate package name
@@ -180,5 +180,3 @@ func main() {
 
 	r.Run(":8000")
 }
-
-
